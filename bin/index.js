a10193019858640704d38e9441033199ce45faad
Luisangonzalez <luisangonzalez@gmail.com>
2018-02-07 20:14:14 +0100

    add support different algorithm and set aes192 for default

--- bin/index.js
+++ bin/index.js
@@ -4,26 +4,24 @@ const program = require('commander');
 const Encrypt = require('../lib/encrypt');
 const Decrypt = require('../lib/decrypt');
 
-
-
 program
     .version('0.0.1')
     .description('Encript and decript files with node');
 
 program
-    .command('encript file password')
+    .command('encript <file> <password> [algorithm]')
     .alias('e')
     .description('Encript file')
-    .action((file, password) => {
-        let encrypt = new Encrypt(file, password);
+    .action((file, password, algorithm) => {
+        let encrypt = new Encrypt(file, password, algorithm);
     });
 
 program
-    .command('decrypt file password')
+    .command('decrypt <file> <password> [algorithm]d')
     .alias('d')
     .description('Decript filet')
-    .action((file, password) => {
-        let decript = new Decrypt(file, password);
+    .action((file, password, algorithm) => {
+        let decript = new Decrypt(file, password, algorithm);
     });   
 
     program.parse(process.argv);
