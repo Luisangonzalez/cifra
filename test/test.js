a9c86f64f9353f286513cd9c8993045e94f9c189
Luis Antonio González Martín <lagonzalez@qdqmedia.com>
2018-02-09 20:42:05 +0100

    add test

--- test/test.js
+++ test/test.js
@@ -1,19 +1,47 @@
-var assert = require('assert');
+const assert = require('assert');
 const Encrypt = require('../lib/encrypt');
 const Decrypt = require('../lib/decrypt');
+const fs = require('fs');
 
+let password;
+
+before(() => {
+    password = Math.random().toString();
+});
 
 describe('Encrypt()', function () {
-    it('should encrypt files ', function () {
-        let encrypt = new Encrypt('test/text', 'manolo');
-        // check if package.json.dec is created
+    it('should encrypt files and generate .dec file ', function () {
+        let encrypt = new Encrypt('test/text', password);
+        assert.equal(fs.existsSync('./test/text.enc'), true);
     });
 });
 
 describe('Decrypt()', function () {
-    it('should decrypt files ', function () {
-        let decrypt = new Decrypt('test/text.enc', 'manolo');
-        // check if package.json.dec is created
-        // assert.equal('./text', './text.enc.dec');
+    it('should decrypt files and generate enc.dec file ', function () {
+        let decrypt = new Decrypt('test/text.enc', password);
+        assert.equal(fs.existsSync('./test/text.enc.dec'), true);
     });
+});
+
+describe('Check content file', function () {
+    let originalFile = fs.readFileSync('./test/text', { encoding: 'utf8' });
+
+    it('should not be equal original content VS encrypt content ', function () {
+        let encryptFile = fs.readFileSync('./test/text.enc', { encoding: 'utf8' });
+        assert.notEqual(originalFile, encryptFile);
+    });
+
+    it('should be equal original content VS decrypt content ', function () {
+        let decryptFile = fs.readFileSync('./test/text.enc.dec', { encoding: 'utf8' });
+        assert.equal(originalFile, decryptFile);
+    });
+});
+
+
+
+after(() => {
+    console.log('Final');
+    if(fs.existsSync('./test/text.enc')) fs.unlinkSync('./test/text.enc');
+    if(fs.existsSync('./test/text.enc.dec')) fs.unlinkSync('./test/text.enc.dec');
+ 
 });
